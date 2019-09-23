a85dd5ed9952dcf97258485a85781e64b533449d
user <jpfs111@gmail.com>
2019-09-23 18:07:24 +0300

    20190923.txt

---
+++ darbi/02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+printf("The best lesson\n");
+printf("The best lesson\r\n");
+printf("\tThe best lesson\n");
+printf("\tThe best lesson\n");
+printf("\'The best lesson\n");
+printf("\"The best lesson\n");
+printf("The best lesson\?\n");
+printf("The best lesson\\/");
+printf("The best lesson\n");
+}
+
+
---
+++ darbi/03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+printf("Hello Universe?\n");
+printf("\\otro rinda\\n");
+}
+
+
